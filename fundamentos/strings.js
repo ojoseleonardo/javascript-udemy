47c557c12285c7846c1eaf12ad0a35dab82852bb
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-09 20:27:11 -0300

    strings

---
+++ fundamentos/strings.js
@@ -0,0 +1,11 @@
+const escola = 'Cod3r'
+
+console.log(escola.charAt(4))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('o'))
+console.log(escola.substring(0,100))
+
+console.log('Escola'.concat(escola).concat("odeio saporra"))
+
+console.log('EscolaeeeEaEaE'.replace('E', 3))
+console.log('AnafodaseMariafodasePedro'.split('fodase'))
