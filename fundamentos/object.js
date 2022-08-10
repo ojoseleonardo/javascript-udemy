b9d8307875f95ae03bc144eef0aefe7ae9fd9736
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-10 11:59:54 -0300

    object

---
+++ fundamentos/object.js
@@ -0,0 +1,12 @@
+const prod1 = {}
+
+prod1.nome = 'Xbox Series S'
+prod1.preco = '3000.00'
+prod1['Desconto Legal'] = 1000
+
+const prod2 =  {
+    nome: 'Playstation 5',
+    preco: '6000.00',
+}
+
+console.log(prod1,prod2)
