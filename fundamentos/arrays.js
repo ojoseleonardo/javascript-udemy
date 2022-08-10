6a85c2edd4e9824a3d038ff2f2a17dbdd81d5e8d
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-10 11:32:41 -0300

    arrays

---
+++ fundamentos/arrays.js
@@ -0,0 +1,14 @@
+const valores = [7.7, 8.0, 6.3 , 9.2]
+console.log(valores[0])
+
+valores[3] =  valores[3] + 10
+console.log(valores[3])
+
+console.log(valores.length)
+
+// console.log(valores.splice(0,1))
+// console.log(valores)
+
+delete valores[3]
+
+console.log(valores)
