59ec0a80754f78bb4d1abd0d31d80e732ea2527c
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-17 13:42:22 -0300

    operadores realcionais

---
+++ fundamentos/relacionais.js
@@ -0,0 +1,8 @@
+// valor igual?
+console.log('1)', '1' == 1)
+// tipo e valor igual?
+console.log('2)', '1' === 1)
+
+console.log('3)', '1' != 1)
+// valor e tipo diferente?
+console.log('4)', '1' !== 1)
