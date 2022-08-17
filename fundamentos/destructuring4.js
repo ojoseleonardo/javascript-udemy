6739d19a34526ee53d704cbe5939be5f3e913acb
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-17 13:40:49 -0300

    detructuring 4

---
+++ fundamentos/destructuring4.js
@@ -0,0 +1,8 @@
+function rand([min = 0, max = 1000]) {
+    if (min > max) [min, max] = [max, min]
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+const obj = [max = 10]
+console.log(rand([50, 50]))
+console.log(rand([obj]))
