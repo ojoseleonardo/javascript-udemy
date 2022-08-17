0f3442033d67a88cc0035b534e1817b8364561ed
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-17 13:42:12 -0300

    par, nome e valor

---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,12 @@
+const Cliente = {
+    nome: 'Pedro',
+    idade: 31,
+    peso: 100,
+    endereco: {
+        logradouro: 'rua foda',
+        numero: 309,
+        complemento: false,
+    }
+}
+
+console.log(Cliente.nome)
