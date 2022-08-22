9442c562d3ebbf91d02f01b5b83439462bb54808
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-22 11:41:08 -0300

    estrutura if 1

---
+++ controle/if1.js
@@ -0,0 +1,10 @@
+function soBoaNoticia(nota) {
+    if (nota >= 7) {
+        console.log('Aprovado com', nota)
+    } else {
+        console.log('Reprovado com', nota)
+    }
+}
+
+soBoaNoticia(6.9)
+soBoaNoticia(7.1)
