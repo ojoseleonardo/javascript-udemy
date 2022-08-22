4dacd5f3dbdce6df750a48df81a540274b7ef772
José Leonardo <joseleonardosantoos@outlook.com>
2022-08-22 12:32:12 -0300

    while

---
+++ controle/while.js
@@ -0,0 +1,12 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+
+}
+
+let opcao = 0
+// enquanto opção foi direfente de 114 voce continua, quando der 114 voce vai parar
+while (opcao != 114) {
+    opcao = getInteiroAleatorioEntre(0, 1000)
+    console.log('Opção gerada foi:', opcao);
+}
